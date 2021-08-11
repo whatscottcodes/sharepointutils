54f97825d2fc64e4c27d9b6edf77b2374890b02c
whatscottcodes <scott.nelsonjr@gmail.com>
2021-08-11 14:23:43 -0400

    updated a typo

--- sputils/utils.py
+++ sputils/utils.py
@@ -23,27 +23,28 @@ class spUtils:
 
         return ctx_auth
     
-    def get_file(self, basesite, spfolderpath, fileName, savePath):
+    def get_file(self, siteName, spFolderPath, fileName, savePath):
     
         filePath = f"{savePath}/{fileName}"
-        siteurl = self.baseurl + basesite 
+        siteurl = self.baseurl + siteName 
         ctx = ClientContext(siteurl, self.ctx_auth)
 
         localpath = filePath
         remotepath = f"{spFolderPath}/{fileName}"
     
         with open(filePath, "wb") as local_file:
-            file = self.ctx.web.get_file_by_server_relative_url(f"{basesite}/{remotepath}").download(local_file).execute_query()
-    
+            file = self.ctx.web.get_file_by_server_relative_url(f"{siteName}/{remotepath}").download(local_file).execute_query()
+
+        print("...file downloaded...")
         return filePath
 
-    def upload_file(self, basesite, spfolderpath, fileName, filePath, first_of_week):
+    def upload_file(self, siteName, spFolderPath, fileName, filePath):
         filePath = f"{filePath}/{fileName}"
-        siteurl = self.baseurl + basesite 
+        siteurl = self.baseurl + siteName 
         ctx = ClientContext(siteurl, self.ctx_auth)
         
         localpath = filePath
-        remotepath = f"{spfolderpath}{fileName}" # existing folder path under sharepoint site.
+        remotepath = f"{spFolderPath}{fileName}" # existing folder path under sharepoint site.
     
         with open(filePath, 'rb') as content_file:
             file_content = content_file.read()
@@ -53,7 +54,7 @@ class spUtils:
         self.ctx.web.get_folder_by_server_relative_url(directory).upload_file(name, file_content).execute_query()
     
         try:
-            self.ctx.web.get_file_by_server_relative_url(f"{basesite}/{remotepath}").checkin(comment=" ", checkin_type=0)
+            self.ctx.web.get_file_by_server_relative_url(f"{siteName}/{remotepath}").checkin(comment=" ", checkin_type=0)
             self.ctx.execute_query()
         except:
             print("...no check-in required...")
