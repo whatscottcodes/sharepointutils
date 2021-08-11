1d3f3730a97cf538da5fd7405f90a65fbee8f5a5
whatscottcodes <scott.nelsonjr@gmail.com>
2021-08-11 14:54:09 -0400

    clarified siteName parameter

--- sputils/utils.py
+++ sputils/utils.py
@@ -26,21 +26,25 @@ class spUtils:
     def get_file(self, siteName, spFolderPath, fileName, savePath):
     
         filePath = f"{savePath}/{fileName}"
-        siteurl = self.baseurl + siteName 
+        fullSiteName = "/sites/" + siteName
+        siteurl = self.baseurl + fullSiteName 
         ctx = ClientContext(siteurl, self.ctx_auth)
-
+        print("YO")
         localpath = filePath
         remotepath = f"{spFolderPath}/{fileName}"
     
         with open(filePath, "wb") as local_file:
-            file = ctx.web.get_file_by_server_relative_url(f"{siteName}/{remotepath}").download(local_file).execute_query()
+            print("HI")
+            print(f"{siteName}/{remotepath}")
+            file = ctx.web.get_file_by_server_relative_url(f"{fullSiteName}/{remotepath}").download(local_file).execute_query()
 
         print("...file downloaded...")
         return filePath
 
     def upload_file(self, siteName, spFolderPath, fileName, filePath):
         filePath = f"{filePath}/{fileName}"
-        siteurl = self.baseurl + siteName 
+        fullSiteName = "/sites/" + siteName
+        siteurl = self.baseurl + fullSiteName 
         ctx = ClientContext(siteurl, self.ctx_auth)
         
         localpath = filePath
@@ -54,7 +58,7 @@ class spUtils:
         ctx.web.get_folder_by_server_relative_url(directory).upload_file(name, file_content).execute_query()
     
         try:
-            ctx.web.get_file_by_server_relative_url(f"{siteName}/{remotepath}").checkin(comment=" ", checkin_type=0)
+            ctx.web.get_file_by_server_relative_url(f"{fullSiteName}/{remotepath}").checkin(comment=" ", checkin_type=0)
             ctx.execute_query()
         except:
             print("...no check-in required...")
